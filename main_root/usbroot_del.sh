b41366931ea4a478714805a753aa0ccb6fc7e1a1
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-10-14 20:34:52 +0200

    Update usbroot_del.sh

--- main_root/usbroot_del.sh
+++ main_root/usbroot_del.sh
@@ -2,4 +2,11 @@
 uci -q delete fstab.extroot
 uci -q delete fstab.rwm
 uci commit fstab
-reboot
+echo "Reinici(A)r? (Pulse A)"
+read -r -n 1 -s key
+if [ "$key" = "A" ]; then
+  echo "reboot..."
+  reboot
+else
+  echo "end"
+fi
