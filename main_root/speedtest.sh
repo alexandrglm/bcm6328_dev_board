9b8010cb250a68b983076f8bcc42dec324ceb728
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-10-14 18:36:53 +0200

    Update speedtest.sh

--- main_root/speedtest.sh
+++ main_root/speedtest.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
-FILE_URL="https://downloads.openwrt.org/releases/23.05.5/targets/bmips/bcm6328/openwrt-23.05.5-bmips-bcm6328-comtrend_ar-5387un-squashfs-sysupgrade.bin"
-FILE_NAME="openwrt-23.05.5-bmips-bcm6328-comtrend_ar-5387un-squashfs-sysupgrade.bin"
-FILE_PATH="/mnt/usb/${FILE_NAME}"
-SERVER_IP="iperf3.moji.fr"
-start_time=$(date +%s)
-wget -O "${FILE_PATH}" "${FILE_URL}"
-end_time=$(date +%s)
-download_time=$((end_time - start_time))
-file_size=$(stat -c%s "${FILE_PATH}")
-download_speed=$(echo "scale=2; ($file_size * 8) / ($download_time * 1000000)" | bc)
-iperf3 -c "${SERVER_IP}" -t "${download_time}" -f m > /tmp/iperf3.log
-write_speed=$(grep "sender" /tmp/iperf3.log | awk '{print $6}')
-rm "${FILE_PATH}"
+URL="http://speedtest.tele2.net/512MB.zip"
 
-echo "Velocidad de descarga: ${download_speed} Mbps"
-echo "Velocidad de escritura: ${write_speed} Mbps"
+FILE="/mnt/usb/1GB.zip"
+
+TEST_FILE="/mnt/usb/test.img"
+
+TEST_FILE_SIZE_MB=1024
+
+TEST_FILE_SIZE_BYTES=$((TEST_FILE_SIZE_MB * 1024 * 1024))
+
+start_time=$(date +%s.%N)
+wget -O "$FILE" "$URL" &
+wget_pid=$!
+
+dd if=/dev/zero of="$TEST_FILE" bs=1M count="$TEST_FILE_SIZE_MB" oflag=direct &
+dd_pid=$!
+
+wait "$wget_pid"
+end_time_wget=$(date +%s.%N)
+
+wait "$dd_pid"
+end_time_dd=$(date +%s.%N)
+
+download_time=$(echo "$end_time_wget - $start_time" | bc)
+
+write_time=$(echo "$end_time_dd - $start_time" | bc)
+
+file_size=$(stat -c%s "$FILE")
+
+download_speed=$(echo "scale=2; $file_size / $download_time / 1024 / 1024" | bc)
+
+write_speed=$(echo "scale=2; $TEST_FILE_SIZE_BYTES / $write_time / 1024 / 1024" | bc)
+
+echo "Tasa de descarga: $download_speed MB/s"
+echo "Tasa de escritura: $write_speed MB/s"
+
+rm "$FILE" "$TEST_FILE"
